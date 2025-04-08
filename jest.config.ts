245d0b48b1d19c20da12ccab41642405c69457b4
yanquankun <yanquankun@58.com>
2025-04-08 20:09:37 +0800

    add: jest

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  testEnvironment: 'jsdom', // 支持 React Hooks 测试
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest', // 使用 ts-jest 处理 TypeScript 文件
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'], // 支持的文件扩展名
+  setupFilesAfterEnv: ['@testing-library/jest-dom'], // 添加 React Testing Library 的扩展
+  collectCoverage: true, // 启用覆盖率报告
+  collectCoverageFrom: [
+    'request/**/*.ts', // 收集 request 文件夹下的覆盖率
+    '!**/node_modules/**', // 排除 node_modules
+    '!**/__test__/**', // 排除测试文件
+  ],
+  coverageDirectory: 'coverage', // 覆盖率报告输出目录
+  testMatch: ['**/__test__/**/*.test.(ts|tsx)'], // 匹配测试文件
+}
+
+export default config
